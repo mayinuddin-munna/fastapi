e0ae4c8f5caf882efd3eefc1bac8474af32a5a27
Munna <mayinuddin.munna@gmail.com>
2026-03-23 12:40:02 +0600

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+
+# Application Configuration
+PORT = 8000
+HOST = "0.0.0.0"
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello DevOps 🚀"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=HOST, port=PORT)
